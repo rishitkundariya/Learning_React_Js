85a8aff0af29998a8fd94f8a5d35a5e39969fdf7
Rishit Kundariya <patelrishit1460@gmail.com>
2023-12-02 10:46:39 +0530

    Added Material UI and custome Style sheet

---
+++ react_js_demo/src/MaterialUi.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+import './style.css'
+export default function MaterialUi(){
+    return (
+        <>
+        <button className="margin-right-20" >This is default button </button>
+        <Button variant="contained">This is from Material Ui. </Button>
+        </>
+    )
+}
--- react_js_demo/src/index.js
+++ react_js_demo/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CustomComponent } from './CustomComponent';
 import { Counter } from './Counter';
+import MaterialUi from './MaterialUi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <CustomComponent/> */}
-    <Counter></Counter>
+    {/* <Counter></Counter> */}
+    <MaterialUi></MaterialUi>
   </React.StrictMode>
 );
